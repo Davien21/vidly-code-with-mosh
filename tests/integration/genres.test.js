0c86d54b0841d6989c9e905e26962e3c2e724859
davien21 <chidiebereekennia@gmail.com>
2020-10-13 22:25:32 +0100

    add test for when genre with given id does not exist

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest')
 const { Genre } = require('../../models/genre');
 const { User } = require('../../models/user');
+const mongoose = require('mongoose')
 
 let server = require('../../app')
 describe('/api/genres', () => {
@@ -18,8 +19,8 @@ describe('/api/genres', () => {
       const res = await request(server).get('/api/genres');
       expect(res.status).toBe(200)
       expect(res.body.length).toBe(2);
-      expect(res.body.some(g => g.name === 'genre1')).toBeTruthy
-      expect(res.body.some(g => g.name === 'genre2')).toBeTruthy
+      expect(res.body.some(g => g.name === 'genre1')).toBeTruthy()
+      expect(res.body.some(g => g.name === 'genre2')).toBeTruthy()
     })
   })
   describe('GET /:id', () => {
@@ -35,6 +36,12 @@ describe('/api/genres', () => {
     })
     it('should return 404 if invalid id is passed', async () => {
       const res = await request(server).get('/api/genres/1');
+
+      expect(res.status).toBe(404);
+    })
+    it('should return 404 if no genre with given id exists', async () => {
+      const id = mongoose.Types.ObjectId()
+      const res = await request(server).get('/api/genres/' + id);
 
       expect(res.status).toBe(404);
     })
