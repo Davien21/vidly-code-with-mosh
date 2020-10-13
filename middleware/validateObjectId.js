3d75e52a245f3df1ab99ed51f46bb884ee7f4c32
davien21 <chidiebereekennia@gmail.com>
2020-10-14 00:39:46 +0100

    remove unnecessary  bad_req variable from module import

--- middleware/validateObjectId.js
+++ middleware/validateObjectId.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const {bad_req, invalid} = require('../util');
+const { invalid } = require('../util');
 
 module.exports = function (req, res, next) {
   if (!mongoose.Types.ObjectId.isValid(req.params.id))
