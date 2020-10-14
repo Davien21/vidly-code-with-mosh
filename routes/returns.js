b2e7be4ac3c130aefa53fa719f983ad3174d567a
davien21 <chidiebereekennia@gmail.com>
2020-10-14 23:16:12 +0100

    add code to pass test for customer and move id tests

--- routes/returns.js
+++ routes/returns.js
@@ -1,7 +1,17 @@
+gitconst auth = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
+const {bad_req, invalid} = require('../util');
+const { User } = require('../models/user')
 
 router.post('/', async (req,res) => {
+  // const {error} = validate(req.body);
+	// if (error) return bad_req(res,error.details[0].message);
+  if (!req.body.customerId) return res.status(400).send('customerId not provided')
+  if (!req.body.movieId) return res.status(400).send('customerId not provided')
+  
+  // if (req.body.customerId && req.body.movieId) return res.status(200).send('good')
+
   res.status(401).send('Unauthorized')
 })
 
