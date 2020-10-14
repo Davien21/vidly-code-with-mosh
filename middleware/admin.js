7d8ec73fd6dadbd5679c5e4356e1ae95b5950c35
davien21 <chidiebereekennia@gmail.com>
2020-10-14 02:00:13 +0100

    update comment on HTTP codes

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,6 +1,7 @@
 module.exports = function (req,res,next) {
+  // Meaning of codes:
   // 401 Unauthorized - invalid token
-  // 403 Forbidden - invalid role access or multiple attempts to enter invalid token
+  // 403 Forbidden - invalid role access (other cases - multiple attempts to enter invalid token)
   if (!req.user.isAdmin) return res.status(403).send('Access denied.');
 
   next();
