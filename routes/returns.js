9167fe46391e9aa85464e8b2e84f4aa0ab8f5f78
davien21 <chidiebereekennia@gmail.com>
2020-10-16 01:30:04 +0100

    refactor with static and instance methods

--- routes/returns.js
+++ routes/returns.js
@@ -1,5 +1,5 @@
 const Joi = require('joi');
-const moment = require('moment');
+const validate = require('../middleware/validate')
 const auth = require('../middleware/auth');
 const express = require('express');
 const router = express.Router();
@@ -7,33 +7,24 @@ const { bad_req, invalid } = require('../util');
 const { Rental } = require('../models/rental')
 const { Movie } = require('../models/movie')
 
-router.post('/', auth, async (req,res) => {
-  const {error} = validate(req.body);
-	if (error) return bad_req(res,error.details[0].message);
-
-  const rental = await Rental.findOne({
-    'customer._id': req.body.customerId, 
-    'movie._id': req.body.movieId, 
-  });
+router.post('/', [auth, validate(validateReturn)], async (req,res) => {
+  const rental = await Rental.lookup(req.body.customerId, req.body.movieId)
 
   if (!rental) return res.status(404).send('Rental not found');
 
   if (rental.dateReturned) return res.status(400).send('Return already processed')
-  
-  rental.dateReturned = new Date();
-  const rentalDays = moment().diff(rental.dateOut, 'days');
-  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
-
+    
+  rental.return();
   await rental.save();
 
   await Movie.update({ _id: rental.movie._id }, {
     $inc: { numberInStock: 1 }
   })
 
-  return res.status(200).send(rental);
+  return res.send(rental);
 })
 
-function validate (returns) {
+function validateReturn (returns) {
 	const schema = {
 		customerId: Joi.objectId().required(),
 		movieId: Joi.objectId().required(),
