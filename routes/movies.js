3777381ea09a1986dbf38e7697527df3100e52a3
davien21 <chidiebereekennia@gmail.com>
2020-10-23 23:51:04 +0100

    correct PUT / route

--- routes/movies.js
+++ routes/movies.js
@@ -39,7 +39,11 @@ router.post('/', auth, async (req,res) => {
 router.put('/:id', auth, async (req,res) => {
 	const {error} = validate(req.body);
 	if (error) return bad_req(res,error.details[0].message);
-	const movie = await Movie.findByIdAndUpdate(req.params.id, { 
+  
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return invalid (res, 'Genre'); 
+
+  const movie = await Movie.findByIdAndUpdate(req.params.id, { 
     title : req.body.title,
     genre: {
       _id: genre._id,
